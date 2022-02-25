60eb9acfaae9935a50c66159c72685fe6e7c3752
godiaz89 <gonzaloit1989@gmail.com>
2022-02-25 14:56:46 -0300

    using msi for aks

--- main.tf
+++ main.tf
@@ -1,15 +1,19 @@
 resource "azurerm_kubernetes_cluster" "tf-px-aks" {
-  name = "tf-px-aks"
-  location = "eastus"
+  name                = "tf-px-aks"
+  location            = "eastus"
   resource_group_name = "pixie-test"
-  dns_prefix = "tfakspx"
+  dns_prefix          = "tfakspx"
 
-    default_node_pool {
-      name = "tfdefault"
-      node_count = 1
-      vm_size = "Standard_B2ms"
+  default_node_pool {
+    name       = "tfdefault"
+    node_count = 1
+    vm_size    = "Standard_B2ms"
+  }
+
+    identity {
+      type = "SystemAssigned"
     }
 
-     
+
 
 }
