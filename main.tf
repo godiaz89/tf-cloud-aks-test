f064399a24565e6d798ed93e28484b4020f219ae
godiaz89 <gonzaloit1989@gmail.com>
2022-02-26 11:50:16 -0300

    changing site location

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "azurerm_kubernetes_cluster" "tf-px-aks" {
 
 resource "azurerm_resource_group" "gonsite" {
   name = "gonwebsite"
-  location = "eastus"
+  location = "eastus2"
 }
 resource "azurerm_static_site" "gonsite" {
   name = "gonsite"
