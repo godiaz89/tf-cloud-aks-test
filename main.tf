14e53ccb1018a575cd6e2c0fc34c479628152791
godiaz89 <gonzaloit1989@gmail.com>
2022-02-26 11:38:38 -0300

    added gonsite

--- main.tf
+++ main.tf
@@ -14,6 +14,25 @@ resource "azurerm_kubernetes_cluster" "tf-px-aks" {
       type = "SystemAssigned"
     }
 
+}
 
 
+
+resource "azurerm_resource_group" "gonsite" {
+  name = "gonwebsite"
+  location = "eastus"
+}
+resource "azurerm_static_site" "mystaticsite" {
+  name = "gonsite"
+  location = azurerm_resource_group.gonsite.location
+  resource_group_name = azurerm_resource_group.gonsite.name
+  sku_tier = "standard"
+  sku_size = "standard"
+
+}
+
+
+output "gonsitedata" {
+  default_host_name = azurerm_static_site.gonsite.default_host_name
+  management_token = azurerm_static_site.gonsite.api_key
 }
